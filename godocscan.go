8fd7285790ba3937e00696128545ad4f570883f3
Estérfano Lopes <esterfano.lopes@gmail.com>
2021-02-23 09:55:07 -0300

    feat: Separate some main funcs for 'registro geral'

--- godocscan.go
+++ godocscan.go
@@ -8,14 +8,17 @@ import (
 )
 
 const (
-	// ddMMMyyyyREGEX to RG document dates
-	ddMMMyyyyREGEX = `(([0-9])|([0-2][0-9])|([3][0-1]))\/(JAN|FEV|MAR|ABR|MAI|JUN|JUL|AGO|SET|OUT|NOV|DEZ)\/\d{4}`
+	// Regex_ddMMMyyyy to RG document dates
+	Regex_ddMMMyyyy = `(([0-9])|([0-2][0-9])|([3][0-1]))\/(JAN|FEV|MAR|ABR|MAI|JUN|JUL|AGO|SET|OUT|NOV|DEZ)\/\d{4}`
 )
 
-func setup() (client *gosseract.Client, err error) {
-	client = gosseract.NewClient()
+func Setup(language string) (client *gosseract.Client, err error) {
+	if language == "" {
+		language = "por"
+	}
 
-	err = client.SetLanguage("por")
+	client = gosseract.NewClient()
+	err = client.SetLanguage(language)
 	if err != nil {
 		return
 	}
@@ -23,41 +26,95 @@ func setup() (client *gosseract.Client, err error) {
 	return
 }
 
-func matchDate(text string) []string {
-	re := regexp.MustCompile(ddMMMyyyyREGEX)
+type RegistroGeral struct {
+	Client    *gosseract.Client
+	Number    string
+	IssueDate string
+	BirthDate string
+	Raw       string
+}
+
+func (rg *RegistroGeral) New(language string) (err error) {
+	rg.Client, err = Setup(language)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (rg *RegistroGeral) SetImage(imagePath string) (err error) {
+	err = rg.Client.SetImage(imagePath)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (rg *RegistroGeral) ScanDates() (err error) {
+	text, err := rg.Client.Text()
+	if err != nil {
+		return
+	}
+
+	result := rg.matchDate(text)
+
+	rg.IssueDate = result[0]
+	rg.BirthDate = result[1]
+
+	return
+}
+
+func (rg *RegistroGeral) ScanRaw() (err error) {
+	rg.Raw, err = rg.Client.Text()
+	return
+}
+
+func (rg *RegistroGeral) ScanAll() (err error) {
+	err = rg.ScanRaw()
+	if err != nil {
+		return err
+	}
+
+	rg.ScanDates()
+	if err != nil {
+		return err
+	}
+
+	return
+}
+
+func (rg *RegistroGeral) matchDate(text string) []string {
+	re := regexp.MustCompile(Regex_ddMMMyyyy)
 
 	return re.FindAllString(text, -1)
 }
 
-func ReadRG(client *gosseract.Client) (result []string, err error) {
-	err = client.SetImage("docs/examples/rgexample.jpg")
-	if err != nil {
-		return
-	}
-	text, err := client.Text()
-	if err != nil {
-		return
-	}
-
-	result = matchDate(text)
-
-	return
-}
-
 func main() {
-	cli, err := setup()
+	rg := RegistroGeral{}
+	defer func() {
+		err := rg.Client.Close()
+		if err != nil {
+			fmt.Errorf(`Error closing client: %s`, err)
+			return
+		}
+	}()
+
+	rg.New("por")
+
+	err := rg.SetImage("docs/examples/rgexample.jpg")
 	if err != nil {
-		fmt.Printf(`Error: %s`, err.Error())
+		fmt.Printf("Error setting image: %s", err)
+		return
 	}
 
-	defer cli.Close()
-
-	result, err := ReadRG(cli)
+	err = rg.ScanAll()
 	if err != nil {
-		fmt.Printf("aaa %s", err)
+		fmt.Printf("Error scanning dates: %s", err)
+		return
 	}
 
-	fmt.Printf(`Results: %v`, result)
-
+	fmt.Printf(`Results: %v`, rg)
 	return
 }
